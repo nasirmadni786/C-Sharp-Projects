7494cd9ead85c6b7a5dadbcdcf47a70f104a68f7
nasirmadni786 <68549542+nasirmadni786@users.noreply.github.com>
2021-06-02 11:45:07 +0500

    Add files via upload

---
+++ FindSum/FindSum/MainForm.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Tech Tips
+ * Date: 02/06/2021
+ * Time: 11:18 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FindSum
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			int x, y , sum;
+			x = Convert.ToInt32(textBox.Text);
+			y = Convert.ToInt32(textBox.Text);
+			sum = x+y;
+				MessageBox.Show("Sum of Given Number is" +sum);
+		}
+	}
+}
