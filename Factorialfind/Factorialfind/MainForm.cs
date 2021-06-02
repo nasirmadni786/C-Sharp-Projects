a1ed72c9bb0eda2fba99fd6ccc24a65fcfa3fe5e
nasirmadni786 <68549542+nasirmadni786@users.noreply.github.com>
2021-06-02 11:44:26 +0500

    Add files via upload

---
+++ Factorialfind/Factorialfind/MainForm.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Tech Tips
+ * Date: 02/06/2021
+ * Time: 10:48 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Factorialfind
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			int n, fact=1;
+			n = Convert.ToInt32(textBox.Text);
+			for( int i = 1; i<=n; i++)
+				
+			{
+			fact= fact *i;
+			}
+			MessageBox.Show("Factorial of " +n+  "is =" + fact);
+		}
+	}
+}
---
+++ Factorialfind/Factorialfind/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Tech Tips
+ * Date: 02/06/2021
+ * Time: 10:48 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace Factorialfind
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
